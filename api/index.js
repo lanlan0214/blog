b4b2933a5fadca8ab8edf24dd3b62954d55e4c1a
Frank141 <https://github.com/lanlan0214/TodayDo.git>
2023-01-12 13:48:52 +0800

    add api, mysqltest posts and post relation

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from "express"
+import postRoutes from "./routes/posts.js"
 
 const app = express()
 
 app.use(express.json())
+app.use("/api/posts", postRoutes)
+
+// http://localhost:8800/test
+// 網址有出現代表有在運作
+// app.get('/test', (req, res) => {
+//   res.json('It works')
+// })
 
 app.listen(8800, () => {
   console.log("Connected!")
